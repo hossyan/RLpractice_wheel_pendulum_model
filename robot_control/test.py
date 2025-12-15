c031deefce56de643fa3e7c6b8cdbfa93b918c53
hossyan <kohossyan@gmail.com>
2025-12-16 00:28:47 +0900

    mjcf_ongoing

--- robot_control/test.py
+++ robot_control/test.py
@@ -12,7 +12,7 @@ data = mujoco.MjData(model)
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         
-        data.ctrl[0] = 5.0 
+        # data.ctrl[0] = 5.0 
 
         mujoco.mj_step(model, data)
 
