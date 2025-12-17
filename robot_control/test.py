aa6bebb595fd77579b64943febb1c6ac2e9fbef6
hossyan <kohossyan@gmail.com>
2025-12-17 13:26:51 +0900

    right_wheel xml complete

--- robot_control/test.py
+++ robot_control/test.py
@@ -9,10 +9,13 @@ xml_path = os.path.join(script_dir, "pendulum.xml")
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 
+output_max = 0.021
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         
-        data.ctrl[0] = 1.0 
+        data.ctrl[0] = 1.0 * output_max
+        data.ctrl[1] = 1.0 * output_max
 
         mujoco.mj_step(model, data)
 
