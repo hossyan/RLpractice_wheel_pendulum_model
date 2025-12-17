b0a3c3c95ae9aa6b180d5f910a86f0ad9a021942
hossyan <kohossyan@gmail.com>
2025-12-17 13:58:56 +0900

    MUJOCO_LOGを削除し、ペンデュラムのXMLファイルのジオメトリ設定を修正。テストスクリプトの制御信号を更新。

--- robot_control/test.py
+++ robot_control/test.py
@@ -15,7 +15,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         
         data.ctrl[0] = 1.0 * output_max
-        data.ctrl[1] = 1.0 * output_max
+        data.ctrl[1] = -1.0 * output_max
 
         mujoco.mj_step(model, data)
 
